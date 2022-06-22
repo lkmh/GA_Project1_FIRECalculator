ae0a553d44c81b57ede8ac23b291ab61b32b1045
Louis Koh <louiskoh1@gmail.com>
2022-06-22 14:30:42 +0800

    solved the chart js function issue

--- main.js
+++ main.js
@@ -10,26 +10,36 @@ function updateInput(){
     const inputDict = {}
     for (let i = 0; i < inputArrLen; i++ ){
         if (document.getElementById(inputArr[i]).value) {
-            inputDict[inputArr[i]] =  document.getElementById(inputArr[i]).value
+            // why does JS take the number as a string not a number
+            inputDict[inputArr[i]] =  parseFloat(document.getElementById(inputArr[i]).value)
             console.log(inputDict)
         }
         
     }
     if (Object.keys(inputDict).length === 9) {
         targetRetireValue = calTarget(inputDict.RetireSpending, inputDict.SWR)
-        console.log("Target",targetRetireValue)
-        const chartArr = calSeries(inputDict.age, inputDict.takeHomePay, inputDict.AnnualSpending, inputDict.CurrentNetworth, inputDict.Inflation, inputDict.IncomeGrowthRate, inputDict.ExpectedReturn)
-        document.getElementById("timeseries").innerText = chartArr[1];
+        // console.log("Target",targetRetireValue)
+        // console.log(inputDict.age, inputDict.takeHomePay, inputDict.AnnualSpending, inputDict.CurrentNetWorth, inputDict.Inflation, inputDict.ExpectedReturn)
+        const chartArr = calSeries(inputDict.age, inputDict.takeHomePay, inputDict.AnnualSpending, inputDict.CurrentNetWorth, inputDict.Inflation, inputDict.ExpectedReturn)
+        
+        console.log("Retirement Reach in",checkNumYear(targetRetireValue, chartArr[1]))
     }
 
 
+}
+
+
+function checkNumYear(target, arr){
+    const isLargeNumber = (element) => element > target
+    return arr.findIndex(isLargeNumber)
 }
 
 function calTarget(RetireSpending, SWR) {
     return RetireSpending/ (SWR/100)
 }
 
-function calSeries(age, takeHomePay, AnnualSpending, CurrentNetworth, Inflation, IncomeGrowthRate, ExpectedReturn) {
+function calSeries(age, takeHomePay, AnnualSpending, CurrentNetworth, Inflation, ExpectedReturn) {
+    console.log("type",typeof(age))
     const netWorthSeries = [CurrentNetworth]
     const ageSeries = [age]
 
@@ -43,9 +53,19 @@ function calSeries(age, takeHomePay, AnnualSpending, CurrentNetworth, Inflation,
         CurrentNetworth = newNetWorth
         age += 1
         ageSeries.push(age)
+        
     }
     return [ageSeries, netWorthSeries]
 }
 
-console.log(2)
-console.log(calSeries(32,60000,24000,100000,2,1,10))
+function inputValue(){
+    const inputArr = ['age','takeHomePay','AnnualSpending','RetireSpending','CurrentNetWorth','SWR','Inflation','IncomeGrowthRate','ExpectedReturn'] 
+    const inputArrLen = inputArr.length 
+    const valueArr = [32,60000,24000,10000,200000,2,2,2,10]
+    for (let i = 0; i < inputArrLen; i++ ){
+        document.getElementById(inputArr[i]).value =  valueArr[i]
+
+
+    }
+    updateInput()
+}

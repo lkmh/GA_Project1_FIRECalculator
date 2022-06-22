534927289eaa9bd0d07fad1a7d02525d9a178912
Louis Koh <louiskoh1@gmail.com>
2022-06-22 18:16:06 +0800

    solve 2 lines on chart

--- main.js
+++ main.js
@@ -21,13 +21,52 @@ function updateInput(){
         // console.log("Target",targetRetireValue)
         // console.log(inputDict.age, inputDict.takeHomePay, inputDict.AnnualSpending, inputDict.CurrentNetWorth, inputDict.Inflation, inputDict.ExpectedReturn)
         const chartArr = calSeries(inputDict.age, inputDict.takeHomePay, inputDict.AnnualSpending, inputDict.CurrentNetWorth, inputDict.Inflation, inputDict.ExpectedReturn)
-        
+
+        createChart(chartArr)
+        document.getElementById("first").innerHTML = "testet"
         console.log("Retirement Reach in",checkNumYear(targetRetireValue, chartArr[1]))
     }
 
 
 }
 
+
+function createChart(data){
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data:{
+        labels: data[0],
+        datasets: [
+        {
+                label: 'NetWorth',
+                data: data[1],
+                borderColor: "#3e95cd",
+                fill: false
+                
+                
+            },
+        {
+                label: 'Fire Target',
+                data: Array(data[1].length).fill(1000000),
+                borderColor: "#8e5ea2",
+                fill: false
+            }
+        ]
+    },
+    options: {
+        interaction:{
+            intersect: false,
+            mode: 'index',
+        },
+        title: {
+            display: true,
+            text: 'Fire Goals'
+        }, //WORK on tooltips - show both networth and detla on the same tip
+    }
+    }
+    )
+}
 
 function checkNumYear(target, arr){
     const isLargeNumber = (element) => element > target
@@ -46,7 +85,7 @@ function calSeries(age, takeHomePay, AnnualSpending, CurrentNetworth, Inflation,
     //  hardcoded retirement age as 92 // 
     const netReturn =  ExpectedReturn-Inflation
     const netSavings = takeHomePay - AnnualSpending
-    while (age < 92) {
+    while (age < 80) {
         newNetWorth = 0 
         newNetWorth = CurrentNetworth * (1+(netReturn/100)) + (netSavings)
         netWorthSeries.push(Math.round(newNetWorth))
@@ -61,7 +100,7 @@ function calSeries(age, takeHomePay, AnnualSpending, CurrentNetworth, Inflation,
 function inputValue(){
     const inputArr = ['age','takeHomePay','AnnualSpending','RetireSpending','CurrentNetWorth','SWR','Inflation','IncomeGrowthRate','ExpectedReturn'] 
     const inputArrLen = inputArr.length 
-    const valueArr = [32,60000,24000,10000,200000,2,2,2,10]
+    const valueArr = [32,60000,24000,36000,200000,2,2,2,10]
     for (let i = 0; i < inputArrLen; i++ ){
         document.getElementById(inputArr[i]).value =  valueArr[i]
 
